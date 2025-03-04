6b18d216929b28df0e2a30a9eea13c00254ca424
majoogarzon <majoo.garzon@gmail.com>
2025-03-03 22:37:29 -0300

    Update registr.js

--- registr.js
+++ registr.js
@@ -28,17 +28,49 @@ const prendas =["camisa", "pantalones", "vestidos"];
 const salida =[];
 
 
-opciones = parseInt (prompt( "Desea ver \n\n1. Camisas \n2. Pantalones \n3. Vestidos \n4. Descuentos "));
+opciones = parseInt (alert( "Desea ver \n\n1. Camisas \n2. Pantalones \n3. Vestidos \n4. Descuentos "));
 
  switch (opciones) {
    case 0: 
-       prompt ("Estas son nuestras camisas disponibles");
+       alert ("Estas son nuestras camisas disponibles");
        break;
        case 1:
-         prompt ("Estos son nuestros pantalones disponibles" );
+         alert ("Estos son nuestros pantalones disponibles" );
          break; 
-   case 2: prompt ("Vestidos en stock");
+   case 2: alert ("Vestidos en stock");
      break;
-   case 3: prompt  ( "Deja tu codigo de descuento!");
+   case 3: alert  ( "Deja tu codigo de descuento!");
  }
  console.log (opciones);
+
+ const inputNombre =document.getElementById ("input.nombre");
+ inputNombre.onchange =()=> console.log;
+ 
+ const usuario= JSON.parse(localStorage.getItem);
+ const ingreso = document.getElementById ("Ingreso");
+ 
+ ingreso.addEventListener("submit", (e) => {
+     e.preventDefault();
+     const usuario = e.target[0].value;
+     const clave = e.target [1].value;
+ 
+  const datos= {
+     usuario,
+     clave
+  }  ;
+  console. log ("Estos son los datos ingresados") 
+ });
+ 
+ const productos = (prendas) => {
+     const card =document.createElement ("");
+     card. className = "card";
+     
+ const productoNuevo= document.createElement ("p");
+ productoNuevo.innerText = prendas.productos; 
+ 
+ const precio = document.createElement("p");
+ precio.innerText = `${prenda.precio}`;
+ 
+ const agregarACarrito =document.createElement("button");
+ agregarACarrito. onclick =() => agregarACarrito (productos.precio)
+ }
